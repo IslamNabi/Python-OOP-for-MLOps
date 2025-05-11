8f7d716102beba088ddbfb0fc82544c2c20360bb
Islam Nabi <islamnabics@gmail.com>
2025-05-11 14:34:13 +0500

    Covered till Chatbook Project

--- oops_project.py
+++ oops_project.py
@@ -11,16 +11,18 @@ class chatbook:
                                 2. Press 2 to SignIn                   
                                 3. Press 3 to write a post
                                 4. Press 4 to message a friend
-                                5. Press any other key to exit""")
+                                5. Press any other key to exit
+                           
+                                -> """)
         
         if user_input == "1":
             self.signup()
         elif user_input == "2":
             self.signin()
         elif user_input == "3":
-            pass
+            self.my_post()
         elif user_input == "4":
-            pass
+            self.sentMsg()
         else:
             exit()
 
@@ -45,5 +47,23 @@ class chatbook:
             else:
                 print("Please enter correct credentials.")
         self.menu()
+    
+    def my_post(self):
+        if self.login == True:
+            txt = input('Enter Your message here: ')
+            print(f"Following content has been posted-> {txt}")
+        else:
+            print("You need Signin first to post something")
+            self.menu()
 
-obj = chatbook()
+    def sentMsg(self):
+        if self.login == True:
+            txt = input("Enter Your Msg: ")
+            frd = input("Whom to send the msg!")
+            print(f"Your msg has been sent to your friend {frd}")
+        else:
+            print("You need to signin first to post something!")
+            print("\n")
+            self.menu()
+
+user1 = chatbook()
--- rough.py
+++ rough.py
@@ -1,18 +1,19 @@
-lst = [1,2,3]
-my_str = "practicing MLOps"
-my_int = 150
+#lst = [1,2,3]
+#my_str = "practicing MLOps"
+#my_int = 150
 
-print(type(lst))
-print(type(my_str))
-print(type(my_int))
+#print(type(lst))
+#print(type(my_str))
+#print(type(my_int))
 
 #lst.clear()
 #print(lst)
 #my_str = my_str.capitalize()
 #print(my_str)
 
-a = 'x'
-b = 'y'
-print(a+b)
+#a = 'x'
+#b = 'y'
+#print(a+b)
 
-
+from oops_project import chatbook
+user2 = chatbook()
