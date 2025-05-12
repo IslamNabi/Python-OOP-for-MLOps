119cd3fedfa5b56ea5063828792fefe63cb45c3c
Islam Nabi <islamnabics@gmail.com>
2025-05-12 13:02:23 +0500

    Encapsulation, getter settre and static method covered

--- rough.py
+++ rough.py
@@ -15,5 +15,34 @@
 #b = 'y'
 #print(a+b)
 
+#from oops_project import chatbook
+# user2 = chatbook()
+
+#lst = [1,2,3]
+
+#function 
+#a1 = len(lst)
+#print(a1)
+
+# method belongs to a class we call method using object
+
 from oops_project import chatbook
+user1 = chatbook()
+print(user1.id)
+
+# using static method directly from class rather than obj 
+chatbook.set_id(10)
 user2 = chatbook()
+print(user2.id)
+
+
+#user2 = chatbook()
+#print(user2.id)
+
+#user3 = chatbook()
+#print(user3.id)
+
+#getter and setter
+#print(user1.get_name())
+#user1.set_name('Islam')
+#print(user1.get_name())
